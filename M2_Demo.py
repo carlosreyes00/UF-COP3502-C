90290901526c48cc21678fd278aeeb836799ebed
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2026-01-15 20:27:00 -0500

    add M2_Demo (first lecture)

---
+++ M2_Demo.py
@@ -0,0 +1,42 @@
+# Module 2 Intro to python
+
+import math
+
+# a = math.floor(77.8)
+# print(a)
+
+
+# # Everything in Python is an Object
+# a = 500
+# b = 500
+# c=a
+# print(a)
+# print(type(a))
+# print(id(a))
+# print(id(b))
+# print(id(c))
+
+
+# Final Grade Calculator
+# quiz_score = float(input("Enter quiz averages: "))
+# lab_score = float(input("Enter lab averages: "))
+# exam_score = float(input("Enter exam score: "))
+# project_score = float(input("Enter project score: "))
+#
+# final_grade = quiz_score*.15 + lab_score*.25 + exam_score*.4 + project_score * .2
+#
+# print(f"The final grade is {final_grade}")
+
+# a = float(input("Enter num 1: "))
+# b = float(input("Enter num 2: "))
+# c = a + b
+# print(c)
+
+# c = a + b
+# print(round(c))
+
+# print("The sum of", a,"and", b, "is", c)
+# print(f"The sum of {{a}} {a:.1f} and {b:.2f} is {c:.3f}")
+
+# Create our first program
+# print('''Hello World!, I'm at UF, my name is Carlos {name}.''')
