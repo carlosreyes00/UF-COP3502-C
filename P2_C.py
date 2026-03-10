e90d2282badcadf4b2a774806f84757168b5bca7
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2026-03-10 09:57:00 -0400

    add P2_C

---
+++ P2_C.py
@@ -0,0 +1,16 @@
+# Translates RLE data into a human-readable representation.
+# For each run, in order, it should display the run length in decimal (1-2 digits);
+# the run value in hexadecimal (1 digit); and a delimiter, ‘:’, between runs.
+# (See examples in the standalone menu section.)
+
+# Ex: to_rle_string([15,15,6,4]) returns the string '15f:64'
+def to_rle_string(rle_data):
+    return -1
+
+
+# Translates a string in human-readable RLE format (with delimiters) into RLE byte data.
+# (Inverse of #7)
+
+# Ex: string_to_rle('15f:64') returns the list of ints [15,15,6,4]
+def string_to_rle(rle_string):
+    return -1
