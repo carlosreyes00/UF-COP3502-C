3fb70215305baa366798c8cfaac9e7f2cf545b32
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2026-04-17 20:43:24 -0400

    Lab12 renamed back :)

---
+++ Lab12.py
@@ -0,0 +1,19 @@
+class Pattern:
+    # data: a list of lists of ints ([[1,2,3], [4,5,6]])
+    # Initializes the class, storing data in an instance variable
+    def __init__(self, data):
+      pass
+
+    # filename: a string (“image.txt”)
+    # Create a PGM image from the data passed to the constructor, and write it to filename.
+    # The image dimensions should match the size of data, and the depth should be 255.
+    def make_image(self, filename):
+          pass
+
+class PatternName(Pattern):
+    # args: Whatever arguments you want your pattern to have.
+    # Creates a pattern as a 2D list of ints from 0 to 255.
+    def __init__(self, args):
+        pass
+
+
